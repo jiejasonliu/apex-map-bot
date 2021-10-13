0bcc2fbb19f50df46e7a7da5551a71e4f228a765
Jie Jason Liu <jasonningliu@gmail.com>
2021-10-13 22:29:12 +0000

    Add keep-alive utilizing UptimeRobot

--- main.py
+++ main.py
@@ -3,28 +3,22 @@ import os
 from discord import Intents
 from discord.ext.commands import Bot
 from discord_slash import SlashCommand
+from keep_alive import keep_alive
 
 client = Bot(command_prefix="!", intents=Intents.default())
 slash = SlashCommand(client, sync_commands=True)
 
+
 @client.event
-async def on_ready():   
-    print(f'Online: {client.user}')   
-    
-# # reload cogs
-# @client.command()
-# async def reload(ctx):
-#     for filename in os.listdir('./cogs'):
-#         if filename.endswith('.py'):
-#             extension = f'cogs.{filename[:-3]}' # strip '.py'
-#             client.unload_extension(extension)
-#             client.load_extension(extension)
-#             print('Reloaded cog:', filename)
+async def on_ready():
+    print(f'Online: {client.user}')
+
 
 # load all cogs when bot starts
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}') # strip '.py'
+        client.load_extension(f'cogs.{filename[:-3]}')  # strip '.py'
         print('Loaded cog:', filename)
 
+keep_alive()
 client.run(os.getenv('TOKEN'))
